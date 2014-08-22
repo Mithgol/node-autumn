61dfc3c0d5251c8d6aa52b7f7b1b5492b3d8ac7f
Mithgol the Webmaster <getgit@mithgol.ru>
2014-08-23 01:47:03 +0400

    initial CLI: check for a missing parameter, output coloured log lines

--- autumn.js
+++ autumn.js
@@ -1,1 +1,25 @@
 #!/usr/bin/env node
+
+var ansi = require('ansi')(process.stdout);
+
+var logOK = function(logString){
+   ansi.black().bg.green().write('  OK  ').reset().write(' ');
+   console.log(logString);
+};
+
+var logFAIL = function(logString){
+   ansi.brightWhite().bg.red().write(' FAIL ').reset().write(' ');
+   console.log(logString);
+};
+
+if( process.argv.length < 3 ){
+   logFAIL('Missing parameter.');
+   logFAIL(
+      "Run `autumn username` to export answers from the given user's account."
+   );
+   process.exit(1);
+}
+
+var userName = process.argv[2];
+
+logOK("Exporting answers from the given user's account: " + userName);
